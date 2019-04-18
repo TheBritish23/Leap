d50da6aeadc4109138e0bf8ff79bf71f96724e81
Soro Cat <sorousha326@gmail.com>
2019-04-18 09:54:03 -0700

    add changes in css

--- js/scrpt.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
-  });
-});
