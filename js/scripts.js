1d7608d1659dc811d9061ea0727674c999aa3288
Soro Cat <sorousha326@gmail.com>
2019-04-22 13:42:13 -0700

    add changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
-  });
-});
-var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// jQuery user interface logic here.
-
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
@@ -26,4 +7,21 @@ var leapYear = function(year) {
   }
 };
 
-// jQuery user interface logic here.
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
